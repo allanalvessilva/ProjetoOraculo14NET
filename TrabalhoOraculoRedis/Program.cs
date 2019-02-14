a62665055559abcafd4d2e2113610c932235f9fd
kadoo <cadu.hd@gmail.com>
2019-02-14 20:22:28 -0200

    alterações com codigo google

--- TrabalhoOraculoRedis/Program.cs
+++ TrabalhoOraculoRedis/Program.cs
@@ -1,17 +1,39 @@
 ﻿using StackExchange.Redis;
 using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
 
 namespace TrabalhoOraculoRedis
 {
     class Program
     {
+        const string url = "https://www.google.com.br/search?q=";
+
         static void Main(string[] args)
         {
-            var redis = ConnectionMultiplexer.Connect("localhost");
+            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+
+
+        }
+
+        async static Task MainAsync(string[] args)
+        {
+            //var pergunta = "quanto é 2 + 2?";
+            //var pergunta = "Qual é a capital do brasil?".Replace(" ","+");
+            //var pergunta = "Quem descobriu o brasil?";
+            // nesse caso nao retiorna nada, poderia procurar no wikipedia, se for omlink dele, clicar e fazer o scrapper da pagina do wikipedia
+            //var pergunta = "Quantas luas tem em saturno?";
+
+
+
+            // ------------------------------
+
+            var redis = ConnectionMultiplexer.Connect("40.122.106.36");
             IDatabase db = redis.GetDatabase();
 
             var sub = redis.GetSubscriber();
-            sub.Subscribe("PERGUNTAS", (ch, msg) =>
+            sub.Subscribe("perguntas", (ch, msg) =>
             {
                 PrepareUrl(msg, db);
             });
@@ -19,24 +41,91 @@ namespace TrabalhoOraculoRedis
             Console.ReadLine();
         }
 
-        public static void PrepareUrl(string question, IDatabase db)
+        public static async void PrepareUrl(string question, IDatabase db)
         {
-            string url = "https://www.google.com/search?q=+%20";
+            //question = "pq: quanto é 1500 + 1500?";
+            string asd = string.Empty;
 
-            string[] aux = question.Split(":");
+            //if (question.Contains("+"))
+            //{
+            //    asd = PerguntaComSoma(question);
+            //}
+            //else
+            //{
 
-            url += aux[1].Replace(" ", "+").Replace("?", "");
 
-            url = url + "%3F";
+                string url = "https://www.google.com/search?q=+%20";
 
+                string[] aux = question.Split(":");
+
+                url += aux[1].Replace(" ", "+").Replace("?", "");
+
+                url = url + "%3F";
+
+
+
+                asd = await BuscaNoGoogle(url);
+            //}
             HashEntry[] resp = new HashEntry[]
             {
-                new HashEntry("ALR", url)
+                new HashEntry("ALR", asd)
             };
 
             db.HashSet(aux[0], resp);
 
-            Console.WriteLine(url);
+            //Console.WriteLine(url);
+            //Console.WriteLine(asd);
+            Console.ReadLine();
+        }
+
+        //private static string PerguntaComSoma(string question)
+        //{
+        //    int number1;
+        //    int number2;
+
+        //    if (int.TryParse(question.Substring(question.IndexOf('+'), (question.IndexOf('?') - question.IndexOf('+'))), out number1))
+        //    {
+        //        if (true)
+        //        {
+
+        //        }
+        //    }
+            
+        //}
+
+        public async static Task<string> BuscaNoGoogle(string url)
+        {
+            var httpClient = new HttpClient();
+
+            var html = await httpClient.GetStringAsync(url);
+
+            //Console.WriteLine(html);
+
+            //Console.ReadLine();
+
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+
+            var node = htmlDocument.DocumentNode.SelectSingleNode("//div[@class='FSP1Dd']");
+
+            if (node == null)
+            {
+                node = htmlDocument.DocumentNode.SelectSingleNode("//div[@class='KpMaL']");
+            }
+
+            if (node == null)
+            {
+                node = htmlDocument.DocumentNode.SelectSingleNode("//span[@id='cwos']");
+            }
+
+            if (node == null)
+            {
+                return "Se fossem com laranjas eu saberia";
+            }
+
+
+
+            return node.InnerHtml;
         }
     }
 }
